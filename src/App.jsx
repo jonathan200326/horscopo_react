b9f8f11295cf0dabe4238762ae8d494042eb33f0
jonathan200326 <72526926+jonathan200326@users.noreply.github.com>
2022-10-02 15:31:40 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Horoscopo from './components/Horoscopo'
+
+const App = () => {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Horoscopo /></>
+  )
+}
+
+export default App
